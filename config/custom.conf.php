a5b728fe8fbd3b9446450dc1e4edb5939c8d3efc
xiaochenniao <374270458@qq.com>
2020-03-21 21:39:48 +0800

    支持主从

--- config/custom.conf.php
+++ config/custom.conf.php
@@ -10,10 +10,14 @@ return array(
     'es' => array(
         'use_search' => true, //是否使用es搜索
         'host' => [
-            'http://127.0.0.1:9200',
+            'http://192.168.56.9:9200',
         ],
         'index' => 'base', //#index的名字不能是大写和下划线开头  index 对应关系型数据（以下简称MySQL）里面的数据库，而不是对应MySQL里面的索引，这点要清楚
         'type' => "base_type" //Elastic 5.x 版可一多个 Elastic 6.x 版只允许每个 Index 包含一个 Type，7.x 版将会彻底移除 Type。
+    ),
+    'slave' => array(
+        'is_slave' => true,
+        'host' => '192.168.56.19' //多个用" , " 隔开
     ),
     'db' => array(
         'charset' => 'utf8',
@@ -22,9 +26,9 @@ return array(
                 'database' => 'basecode',
                 'user' => 'root',
                 'pass' => '123456',
-                'host' => 'localhost',
+                'host' => '192.168.56.9',
                 'port' => '3306',
-            ),
+            )
         ),
     ),
 );
